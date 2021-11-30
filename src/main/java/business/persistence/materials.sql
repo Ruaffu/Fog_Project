bdad02b2fec940f43536896d68a601bbc2352fab
LeoNielsen <leo.nielsen21@gmail.com>
2021-11-30 15:37:26 +0100

    fixed prices

--- src/main/java/business/persistence/materials.sql
+++ src/main/java/business/persistence/materials.sql
@@ -1,44 +1,44 @@
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('97x97 mm. trykimp. Stolpe','trykimp. Stolpe','99.54','248.85','3000','97','97','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('97x97 mm. trykimp. Stolpe','trykimp. Stolpe','155.53','248.85','3000','97','97','stk');
 
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','86.34','215.85','3000','25','200','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','103.61','259.03','3600','25','200','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','120.88','302.19','4200','25','200','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','138.14','345.36','4800','25','200','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','155.41','388.53','5400','25','200','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','191.88','479.70','6000','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','134.91','215.85','3000','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','161.89','259.03','3600','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','188.87','302.19','4200','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','215.85','345.36','4800','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','242.83','388.53','5400','25','200','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x200 mm. trykimp. Brædt','trykimp. Brædt','299.81','479.70','6000','25','200','stk');
 
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','57.54','143.85','3000','25','125','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','69.05','172.63','3600','25','125','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','80.56','201.39','4200','25','125','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','92.06','230.16','4800','25','125','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','103.97','258.93','5400','25','125','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','131.88','329.70','6000','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','89.91','143.85','3000','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','187.89','172.63','3600','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','125.87','201.39','4200','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','143.85','230.16','4800','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','161.83','258.93','5400','25','125','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('25x125 mm. trykimp. Brædt','trykimp. Brædt','206.06','329.70','6000','25','125','stk');
 
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','127.2','318','3000','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','152.64','381.60','3600','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','178.08','445.20','4200','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','203.52','508.80','4800','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','228.96','572.40','5400','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','357.6','894','6000','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','393.36','983.40','6600','45','195','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','429.12','1.072.80','7200','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','198.75','318','3000','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','238.50','381.60','3600','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','278.25','445.20','4200','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','318.00','508.80','4800','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','358.00','572.40','5400','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','558.75','894.00','6000','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','614.63','983.40','6600','45','195','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('45x195 mm. spærtræ ubh.','spærtræ ubh','670.50','1072.80','7200','45','195','stk');
 
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','16.52','41.31','1800','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','19.28','48.20','2100','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','22.03','55.08','2400','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','24.78','61.96','2700','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','27.54','68.85','3000','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','33.05','82.63','3600','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','38.56','96.39','4200','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','44.06','110.16','4800','19','100','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','49.57','123.93','5400','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','25.82','41.31','1800','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','30.13','48.20','2100','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','34.43','55.08','2400','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','38.73','61.96','2700','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','43.03','68.85','3000','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','51.64','82.63','3600','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','60.24','96.39','4200','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','68.85','110.16','4800','19','100','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('19x100 mm. trykimp. Brædt','trykimp. Brædt','77.46','123.93','5400','19','100','stk');
 
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','45.6','114','2400','0','1090','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet ','TRAPEZPLADE','59.2','148','3000','0','1090','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','67.6','169','3600','0','1090','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','81.6','204','4200','0','1090','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','91.2','228','4800','0','1090','stk');
-INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','113.6','284','6000','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','71.25','114.00','2400','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet ','TRAPEZPLADE','92.50','148.00','3000','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','105.63','169.00','3600','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','127.50','204.00','4200','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','142.50','228.00','4800','0','1090','stk');
+INSERT INTO fog.material (name,type,cost,price,length,height,width,unit) VALUES ('Plastmo Ecolite blåtonet','TRAPEZPLADE','177.50','284.00','6000','0','1090','stk');
 
 
 
