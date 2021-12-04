3b27b74caccf936c61f10e2af004fcbf9f372734
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2021-12-04 16:43:43 +0100

    SVG class added in services

---
+++ src/main/java/business/services/SVG.java
@@ -0,0 +1,85 @@
+package business.services;
+
+public class SVG
+{
+    StringBuilder svg = new StringBuilder();
+    private int x;
+    private int y;
+    private String viewBox;
+    private int width;
+    private int height;
+
+    private final String headerTemplate = "<svg height=\"%d%%\" " +
+            "width=\"%d%%\" " +
+            "viewBox=\"%s\" " +
+            "x=\"%d\" " +
+            "y=\"%d\" " +
+            " preserveAspectRatio=\"xMinYMin\">";
+
+    private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000000; fill: #ffffff\"/>";
+    private final String lineTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"black\" style=\"stroke-dasharray: 4 4\"/>";
+    private final String arrowTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#000000; fill: #ffffff;marker-start: url(#beginArrow); marker-end: url(#endArrow)\"/>";
+    private final String textTemplate = "<text style=\"text-anchor: middle\" transform=\"translate(%d, %d) rotate(%d)\">%d %s</text>";
+
+    private final String markerTemplate = "<defs>\n" +
+            "        <marker\n" +
+            "                id=\"beginArrow\"\n" +
+            "                markerWidth=\"12\"\n" +
+            "                markerHeight=\"12\"\n" +
+            "                refX=\"0\"\n" +
+            "                refY=\"6\"\n" +
+            "                orient=\"auto\">\n" +
+            "            <path d=\"M0,6 L12,0 L12,12 L0,6\" style=\"fill: #000000;\"/>\n" +
+            "        </marker>\n" +
+            "        <marker\n" +
+            "                id=\"endArrow\"\n" +
+            "                markerWidth=\"12\"\n" +
+            "                markerHeight=\"12\"\n" +
+            "                refX=\"12\"\n" +
+            "                refY=\"6\"\n" +
+            "                orient=\"auto\">\n" +
+            "            <path d=\"M0,0 L12,6 L0,12 L0,0 \" style=\"fill: #000000;\"/>\n" +
+            "        </marker>\n" +
+            "    </defs>";
+
+    public SVG(int x, int y, String viewBox, int width, int height)
+    {
+        this.x = x;
+        this.y = y;
+        this.viewBox = viewBox;
+        this.width = width;
+        this.height = height;
+        svg.append(String.format(headerTemplate,height,width,viewBox,x,y));
+    }
+
+    public void addRect(int x, int y, int height, int width)
+    {
+        svg.append(String.format(rectTemplate,x,y,height,width));
+    }
+
+    public void addLine(int x1, int y1, int x2, int y2)
+    {
+        svg.append(String.format(lineTemplate,x1,y1,x2,y2));
+    }
+
+    public void addArrow(int x1, int y1, int x2, int y2)
+    {
+        svg.append(String.format(arrowTemplate,x1,y1,x2,y2));
+    }
+
+    public void addMarker()
+    {
+        svg.append(String.format(markerTemplate));
+    }
+
+    public void addSVG(SVG innerSVG)
+    {
+        svg.append(innerSVG.toString());
+    }
+
+    @Override
+    public String toString()
+    {
+        return svg.toString() + "</svg>";
+    }
+}
